cfa389e89c5b8a704092cd19b7bdbb7a5b595742
fawazbayureksa <fawwazbayureksa@gmail.com>
2026-01-10 15:54:52 +0700

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  money-manage
+//
+//  Created by Fawwaz Bayureksa on 10/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ money_manageApp.swift
@@ -0,0 +1,17 @@
+//
+//  money_manageApp.swift
+//  money-manage
+//
+//  Created by Fawwaz Bayureksa on 10/01/26.
+//
+
+import SwiftUI
+
+@main
+struct money_manageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
